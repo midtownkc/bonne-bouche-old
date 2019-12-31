8ec15b5ac2d662f08b97c9526d3f591c2a008605
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2019-12-30 22:42:43 -0600

    separate application views. make recipde index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'recipes#index'
+
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
